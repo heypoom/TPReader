d6181f7ad748055b773533837b0924b6b7b319af
Phoomparin Mano <work@phoom.in.th>
2017-10-06 20:48:25 +0700

    :art: Added Landing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,22 +1,62 @@
 <template lang="pug">
   .root
-    h2 Hello!
-    <router-link :to="path" v-for="path in paths" :key="path">{{path}}</router-link>
+    .container
+      h2 Tap Reader
+      input(v-model="link" placeholder="Paste Your taptaptap.co URL" @keyup.enter="go")
+      <router-link :to="path" :key="path">{{path}}</router-link>
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data: () => ({
-      paths: [
-        "853e14eb-b1f3", "7022b57c-be7a", "a8f63b84-b5a2", "9e28c2a4-b6b0",
-        "4d0ec276-d6dc", "-Kmj-b9lcLgKliiJX-R5", "-Km11G7_58KKweqM6hME",
-        "8f7343c0-cda0"
-      ]
-    })
+const parse = async link => link.replace("https://taptaptap.co/story/", "").replace(/\?.*/g, "")
+
+export default {
+  name: "app",
+  data: () => ({
+    path: "",
+    link: ""
+  }),
+  methods: {
+    async go() {
+      this.$router.push(await parse(this.link))
+    }
   }
+}
 </script>
 
 <style lang="sass" scoped>
+  .root
+    text-align: center
+    margin: 0 auto
+    max-width: 600px
+    min-height: 100vh
 
+  .container
+    display: flex
+    flex-direction: column
+    align-items: center
+    justify-content: center
+    min-height: 100vh
+
+  h2
+    font-weight: 300
+    font-family: 'Roboto'
+    font-size: 1.8em
+    color: #777
+
+  input
+    background: #ffffff
+    border: none
+    border-bottom: 2px solid #555
+    padding: 0.3em 1em
+    font-size: 1.2em
+    font-family: Roboto, Sukhumvit Set, Helvetica Neue, sans-serif
+    font-weight: 300
+    outline: none
+    color: #333
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)
+    transition: 1s cubic-bezier(0.22, 0.61, 0.36, 1) all
+
+    &:hover
+      transform: scale(1.1)
+      border-bottom: 2px solid #9b59b6
 </style>
